89ae24b07e31dc9d6951b1232b1f946370530347
Carlos Vinicius <94083688+Skitttz@users.noreply.github.com>
2023-09-23 20:43:39 -0300

    change msg helper error

--- src/Components/Helper/Error.jsx
+++ src/Components/Helper/Error.jsx
@@ -3,7 +3,8 @@ import styles from "./Error.module.css";
 
 const Error = ({ error }) => {
   if (error === "Failed to fetch") {
-    error = "Falha com o servidor, tente novamente mais tarde.";
+    error =
+      "Ops! ocorreu um problema ao buscar os dados. Por favor, tente novamente mais tarde.";
   }
   if (!error) return null;
   return <p className={`${styles.errorText} animeDown`}>{error}</p>;
