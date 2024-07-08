9b023f94388efa6ae7bfb1e6536d02889d977344
SKitttz <skittz.dev@gmail.com>
2024-07-07 23:43:46 -0300

    refact: add Logout Modal

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,9 +5,11 @@ import Cats from '../Assets/cats.svg';
 import MinhaConta from '../Assets/usuario.svg';
 import Sair from '../Assets/sair.svg';
 import { useUser } from '../UserContext';
+import UserLogoutModal from './User/UserLogoutModal';
 
 const Header = () => {
   const { data, userLogout } = useUser();
+  const [modalLogout, setModalLogout] = React.useState(null);
   const navigate = useNavigate();
 
   function handleLogout() {
@@ -15,35 +17,46 @@ const Header = () => {
     navigate('/login');
   }
   return (
-    <header className={styles.header}>
-      <nav className={`${styles.nav} container`}>
-        <Link className={styles.logo} to="/" aria-label="Cats - Home">
-          <Cats />
-        </Link>
-        {data ? (
-          <div className={styles.loginContainer}>
-            <p className={styles.paragraphName}>
-              Olá,{' '}
-              <span style={{ color: '#333' }} className={styles.nome}>
-                {data.nome[0].toUpperCase() + data.nome.substring(1)}
-              </span>
-            </p>
-            <Link className={styles.login} to="/conta">
-              <button className={styles.btnMinha}>
-                <MinhaConta />
-              </button>
-            </Link>
-            <button className={styles.sair} onClick={handleLogout}>
-              <Sair />
-            </button>
-          </div>
-        ) : (
-          <Link className={styles.login} to="/login">
-            Login / Criar
+    <>
+      <header className={styles.header}>
+        <nav className={`${styles.nav} container`}>
+          <Link className={styles.logo} to="/" aria-label="Cats - Home">
+            <Cats />
           </Link>
-        )}
-      </nav>
-    </header>
+          {data ? (
+            <div className={styles.loginContainer}>
+              <p className={styles.paragraphName}>
+                Olá,{' '}
+                <span style={{ color: '#333' }} className={styles.nome}>
+                  {data.nome[0].toUpperCase() + data.nome.substring(1)}
+                </span>
+              </p>
+              <Link className={styles.login} to="/conta">
+                <button className={styles.btnMinha}>
+                  <MinhaConta />
+                </button>
+              </Link>
+              <button
+                className={styles.sair}
+                onClick={() => setModalLogout(true)}
+              >
+                <Sair />
+              </button>
+            </div>
+          ) : (
+            <Link className={styles.login} to="/login">
+              Login / Criar
+            </Link>
+          )}
+        </nav>
+      </header>
+      {modalLogout && (
+        <UserLogoutModal
+          setModalLogout={setModalLogout}
+          handleLogout={handleLogout}
+        />
+      )}
+    </>
   );
 };
 
