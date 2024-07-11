3857fbc09a054220e29f9051f9c1d76ca93becd2
SKitttz <skittz.dev@gmail.com>
2024-07-10 23:48:56 -0300

    feat: currentPathProfile

---
+++ src/Components/Utils/CurrentRoute.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+
+export function CurrentPathProfileUser() {
+  const location = useLocation();
+  const currentPathname = location.pathname;
+  if (
+    currentPathname.startsWith('/perfil') ||
+    currentPathname.startsWith('/conta')
+  )
+    return true;
+
+  return false;
+}
