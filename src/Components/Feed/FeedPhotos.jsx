5571b04f584abc2b3cd416883d14a42d66e23400
SKitttz <skittz.dev@gmail.com>
2024-07-10 21:40:30 -0300

    refact: implement new response get info user

--- src/Components/Feed/FeedPhotos.jsx
+++ src/Components/Feed/FeedPhotos.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import FeedPhotosItem from './FeedPhotosItem';
-import { PHOTOS_GET } from '../../Api';
+import { PHOTOS_GET, USER_GET_INFO_NAME } from '../../Api';
 import useFetch from '../../Hooks/useFetch';
 import Error from '../Helper/Error';
 import Loading from '../Helper/Loading';
@@ -11,6 +11,19 @@ import AddPhotosSvg from '../../Assets/adicionar.svg';
 
 const FeedPhotos = ({ page, user, setModalPhoto, setInfinite }) => {
   const { data, loading, error, request } = useFetch();
+  const [userInfo, setUserInfo] = React.useState(null);
+  React.useEffect(() => {
+    async function fetchUserInfo() {
+      try {
+        const name = await USER_GET_INFO_NAME();
+        setUserInfo(name);
+      } catch (error) {
+        console.error('[Error User]');
+        setUserInfo(null);
+      }
+    }
+    fetchUserInfo();
+  }, []);
 
   React.useEffect(() => {
     async function fetchPhotos() {
@@ -23,27 +36,42 @@ const FeedPhotos = ({ page, user, setModalPhoto, setInfinite }) => {
     }
     fetchPhotos();
   }, [request, user, page, setInfinite]);
-
   if (error) return <Error error={error} />;
   if (loading) return <Loading />;
-  if ((data === null || data.length === 0) && user !== 0)
-    return (
-      <div className={`${styles.noPostUser} animeOpacity`}>
-        <div>
-          <p>Ops! Parece que seu estoque de gatinhos está vazio.</p>
-          <p>Ainda não existem postagens para exibir.</p>
+  if ((data === null || data.length === 0) && user !== 0) {
+    if (
+      (typeof user === 'string' && user === userInfo?.name) ||
+      (typeof user === 'number' && user === userInfo?.id)
+    ) {
+      return (
+        <div className={`${styles.noPostUser} animeOpacity`}>
+          <div>
+            <p>Ops! Parece que seu estoque de gatinhos está vazio.</p>
+            <p>Ainda não existem postagens para exibir.</p>
+          </div>
+          <div className={`${styles.containerButtons}`}>
+            <NavLink to={'/conta/post'}>
+              <Button>Criar primeiro post 🐾</Button>
+            </NavLink>
+            ou selecione
+            <NavLink to={'/conta/post'} aria-label="Icone Nav">
+              <AddPhotosSvg />
+            </NavLink>
+          </div>
         </div>
-        <div className={`${styles.containerButtons}`}>
-          <NavLink to={'/conta/post'}>
-            <Button>Criar primeiro post 🐾</Button>
-          </NavLink>
-          ou selecione
-          <NavLink to={'/conta/post'} aria-label="Icone Nav">
-            <AddPhotosSvg />
-          </NavLink>
+      );
+    } else {
+      return (
+        <div className={`${styles.noPostUser} animeOpacity`}>
+          <div>
+            <p>Ops! Parece que este usuário ainda não compartilhou nadinha.</p>
+            <p>Ainda deve estar criando novidades fofinhas... 😺✨</p>
+          </div>
         </div>
-      </div>
-    );
+      );
+    }
+  }
+
   if (data)
     return (
       <ul className={`${styles.feed} animeLeft`}>
