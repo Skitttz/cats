8d0f5bdba715ea2d9e9cd296d266d2771a4d3bb8
SKitttz <skittz.dev@gmail.com>
2024-07-17 11:04:00 -0300

    refact: remove message token

--- src/Components/Helper/Error.jsx
+++ src/Components/Helper/Error.jsx
@@ -1,12 +1,12 @@
-import React from "react";
-import styles from "./Error.module.css";
+import React from 'react';
+import styles from './Error.module.css';
 
 const Error = ({ error }) => {
-  if (error === "Failed to fetch") {
+  if (error === 'Failed to fetch') {
     error =
-      "Ops! ocorreu um problema ao buscar os dados. Por favor, tente novamente mais tarde.";
+      'Ops! ocorreu um problema ao buscar os dados. Por favor, tente novamente mais tarde.';
   }
-  if (!error) return null;
+  if (!error || error === 'Token Inválido') return null;
   return <p className={`${styles.errorText} animeDown`}>{error}</p>;
 };
 
